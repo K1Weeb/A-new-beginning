8a593b147fc87feb6096ab0c687d2236561249c9
stevensc9277 <131869852+K1Weeb@users.noreply.github.com>
2023-04-26 22:55:19 +1200

    added comments to explain some new code

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ import random
 
 class Start:
     def __init__(self, parent):
-        print("hello world")
+        
+        # initialize starting frame
         self.main_frame = Frame(padx=10, pady=10, height=100, width=100)
         self.main_frame.grid()
 
-        self.text_label = Label(self.main_frame, text="I don't remember sh*t", padx=10, pady=10)
+        # added labels which will contain text
+        self.text_label = Label(self.main_frame, font="arial 11", text="I don't remember sh*t", padx=10, pady=10)
         self.text_label.grid(row=0)
 
 
@@ -16,6 +18,9 @@ class Start:
 if __name__ == "__main__":
     root = Tk()
     root.title("A New Beginning")
-    root.geometry("500x100")
+
+    # adjust window size (widthxheight+abscissa+ordinate)
+    root.geometry("500x100+400+300")
+
     something = Start(root)
     root.mainloop()
